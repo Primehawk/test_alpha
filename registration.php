87217926dcc7b0fdd08c0ee180b140f1da9f4c18
Tom <tprotogerakis@netsteps.gr>
2024-12-14 12:30:31 +0200

    rename module

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Primehawk_ModuleB',
+    'Primehawk_Alpha',
     __DIR__
 );
